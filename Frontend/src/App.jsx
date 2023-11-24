c3f61eadc2b5b106af098a923bcdcacfc3d7b975
sohancwact <sohancwactfw@gmail.com>
2023-11-24 08:30:43 +0545

    making changes in code and folder-structure

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,21 +1,20 @@
-
-import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Trips from './trip/pages/Trips';
-import MainNavigation from './shared/components/Navigation/MainNavigation';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Trips from "./pages/Trips";
+import MainNavigation from "./Components/Navigation/MainNavigation";
+import About from "./pages/About";
 
 function App() {
-
   return (
-  <Router>
-    
-    <MainNavigation />
-    <Routes>          
-    <Route path='/' exact Component={Trips}>
-    </Route>
-    </Routes>
-  </Router>    
+    <>
+      <Router>
+        {/*   <MainNavigation /> */}
+        <Routes>
+          <Route path="/" exact Component={Trips} />
+          <Route path="/about" exact element={<About />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
-export default App
+export default App;
--- Frontend/src/trip/pages/NewTrip.js
+++
@@ -1,9 +0,0 @@
-// import React from "react";
-
-const NewTrip = () => {
-
-    return <h2>I am New Trip !</h2>
-    
-};
-
-export default NewTrip;
