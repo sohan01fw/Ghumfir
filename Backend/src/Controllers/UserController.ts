106a6e70b696626b5da4b73578aba0148f945927
sohancwact <sohancwactfw@gmail.com>
2023-12-14 20:16:44 +0545

    changes on route handling file

--- Backend/src/Controllers/UserController.ts
+++ Backend/src/Controllers/UserController.ts
@@ -1,8 +1,9 @@
 import { UserModel } from "../Db/Models/User.model.ts";
 import { User } from "../../types";
+import { Request, Response } from "express";
 
 //user creation
-export default async function UserCreate(req: any, res: any) {
+export async function UserCreate(req: Request, res: Response) {
   try {
     const { userId, email, name, itineraries }: User = req.body;
     const findUser = false;
@@ -24,5 +25,14 @@ export default async function UserCreate(req: any, res: any) {
   } catch (error) {
     console.error(error);
     res.status(500).json({ msg: "Error while processing user" });
+  }
+}
+
+export async function handleDeleteUser(req: Request, res: Response) {
+  try {
+    const { userId } = req.body;
+    console.log(userId);
+  } catch (error) {
+    res.status(500).json({ msg: "error while deleting user" });
   }
 }
