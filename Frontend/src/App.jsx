2b8b59bc4638d0158d6d2e73d6af1320ea11506c
sohancwact <sohancwactfw@gmail.com>
2023-12-21 21:01:37 +0545

    integrating map

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,12 +2,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Trips from "./Components/TipsDetails/Trips";
 import MainNavigation from "./Components/Navigation/MainNavigation";
 import About from "./pages/Home/About";
-// import GoogleMap from "./Components/map/GoogleMap";
 import { TripFormProvider } from "./Contexts/TripFormContext";
 import Footer from "./Components/Footer/Footer";
 import Auth from "./pages/Auth/Auth";
 import Blog from "./pages/Home/Blog";
 import TripDetails from "./Components/TipsDetails/TripDetails";
+import MapView from "./pages/Home/MapView";
 
 function App() {
   return (
@@ -22,6 +22,7 @@ function App() {
               <Route path="/tripDetails" exact element={<TripDetails />} />
               <Route path="/Blog" exact element={<Blog />} />
               <Route path="/auth" element={<Auth />} />
+              <Route path="/map" element={<MapView />} />
             </Routes>
           </div>
           <Footer />
