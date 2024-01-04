9105aa21c7ead347152948d6e065814ae939ed45
Samir Gautam <samirgautam003@gmail.com>
2024-01-04 10:35:15 +0545

    updating trip details section

--- Frontend/src/Components/TipsDetails/Budget.jsx
+++ Frontend/src/Components/TipsDetails/Budget.jsx
@@ -3,9 +3,65 @@ const Budget = () => {
  
 
   return (
-    <div>
+    <div className="budget" id="budget">
       <h2>Budget</h2>
-     
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi labore
+      ullam modi praesentium inventore repudiandae quas, fugiat saepe non
+      architecto placeat earum sapiente minima error? Sequi eligendi tempora
+      atque ut! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi labore
+      ullam modi praesentium inventore repudiandae quas, fugiat saepe non
+      architecto placeat earum sapiente minima error? Sequi eligendi tempora
+      atque ut! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi labore
+      ullam modi praesentium inventore repudiandae quas, fugiat saepe non
+      architecto placeat earum sapiente minima error? Sequi eligendi tempora
+      atque ut! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi labore
+      ullam modi praesentium inventore repudiandae quas, fugiat saepe non
+      architecto placeat earum sapiente minima error? Sequi eligendi tempora
+      atque ut! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi labore
+      ullam modi praesentium inventore repudiandae quas, fugiat saepe non
+      architecto placeat earum sapiente minima error? Sequi eligendi tempora
+      atque ut! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi labore
+      ullam modi praesentium inventore repudiandae quas, fugiat saepe non
+      architecto placeat earum sapiente minima error? Sequi eligendi tempora
+      atque ut!
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi labore
+      ullam modi praesentium inventore repudiandae quas, fugiat saepe non
+      architecto placeat earum sapiente minima error? Sequi eligendi tempora
+      atque ut! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi labore
+      ullam modi praesentium inventore repudiandae quas, fugiat saepe non
+      architecto placeat earum sapiente minima error? Sequi eligendi tempora
+      atque ut! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi labore
+      ullam modi praesentium inventore repudiandae quas, fugiat saepe non
+      architecto placeat earum sapiente minima error? Sequi eligendi tempora
+      atque ut! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi labore
+      ullam modi praesentium inventore repudiandae quas, fugiat saepe non
+      architecto placeat earum sapiente minima error? Sequi eligendi tempora
+      atque ut! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi labore
+      ullam modi praesentium inventore repudiandae quas, fugiat saepe non
+      architecto placeat earum sapiente minima error? Sequi eligendi tempora
+      atque ut! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi labore
+      ullam modi praesentium inventore repudiandae quas, fugiat saepe non
+      architecto placeat earum sapiente minima error? Sequi eligendi tempora
+      atque ut!
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi labore
+      ullam modi praesentium inventore repudiandae quas, fugiat saepe non
+      architecto placeat earum sapiente minima error? Sequi eligendi tempora
+      atque ut! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi labore
+      ullam modi praesentium inventore repudiandae quas, fugiat saepe non
+      architecto placeat earum sapiente minima error? Sequi eligendi tempora
+      atque ut! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi labore
+      ullam modi praesentium inventore repudiandae quas, fugiat saepe non
+      architecto placeat earum sapiente minima error? Sequi eligendi tempora
+      atque ut! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi labore
+      ullam modi praesentium inventore repudiandae quas, fugiat saepe non
+      architecto placeat earum sapiente minima error? Sequi eligendi tempora
+      atque ut! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi labore
+      ullam modi praesentium inventore repudiandae quas, fugiat saepe non
+      architecto placeat earum sapiente minima error? Sequi eligendi tempora
+      atque ut! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi labore
+      ullam modi praesentium inventore repudiandae quas, fugiat saepe non
+      architecto placeat earum sapiente minima error? Sequi eligendi tempora
+      atque ut!
     </div>
   );
 };
