b8128cf9617709cfebedc4e8c237292a5a8d4215
sohancwact <sohancwactfw@gmail.com>
2024-03-04 22:22:29 +0545

    features to add placeItiDetails

--- Backend/src/routes/placeDetailsRoute.ts
+++ Backend/src/routes/placeDetailsRoute.ts
@@ -7,9 +7,7 @@ import express from "express";
 
 export const placeDetailsRoute = express.Router();
 //inserting all itiDetails in db
-placeDetailsRoute
-  .route("/insertPlaceItiId/:pId/:itiId")
-  .post(insertAllItiDetails);
+placeDetailsRoute.route("/insertPlaceItiId/:itiId").post(insertAllItiDetails);
 
 placeDetailsRoute
   .route("/getItiPlacesDetails/:itiId")
