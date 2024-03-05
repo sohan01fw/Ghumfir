d3a87330e2f63dd549dd5ccc638e58f1e9ed185a
Samir Gautam <samirgautam003@gmail.com>
2024-03-05 16:11:31 +0545

    Home Page Styling UI

--- Frontend/src/Components/Footer/Footer.jsx
+++ Frontend/src/Components/Footer/Footer.jsx
@@ -1,9 +1,7 @@
-import { Box, Flex, Text} from "@chakra-ui/react";
-import { useLocation, Link} from "react-router-dom";
+import { Flex, Text, Box } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-  const { pathname } = useLocation();
-
   return (
     <Flex
       as="footer"
@@ -12,10 +10,8 @@ const Footer = () => {
       textAlign="center"
       p={4}
       boxShadow="0 2px 6px rgba(0, 0, 0, 0.26)"
-      position="fixed"
-      bottom="0"
       width="100%"
-      zIndex="999"
+      mt="auto"
     >
       <Flex
         justifyContent="space-between"
@@ -31,9 +27,9 @@ const Footer = () => {
         <Box flex={1} textAlign={{ base: "center", md: "left" }} mb={{ base: 4, md: 0 }}>
           <Text fontSize="xl" fontWeight="bold" mb={2}>Quick Links</Text>
           <Flex flexDirection="column">
-            <Link to="/" color={pathname === "/" ? "blue.500" : "gray.600"} textDecoration="none" mb={2}>Home</Link>
-            <Link to="/about" color={pathname === "/about" ? "blue.500" : "gray.600"} textDecoration="none" mb={2}>About Us</Link>
-            <Link to="/Blog" color={pathname === "/Blog" ? "blue.500" : "gray.600"} textDecoration="none">Blog</Link>
+            <Link to="/" textDecoration="none" mb={2}>Home</Link>
+            <Link to="/about" textDecoration="none" mb={2}>About Us</Link>
+            <Link to="/blog" textDecoration="none">Blog</Link>
           </Flex>
         </Box>
         <Box flex={1} textAlign={{ base: "center", md: "left" }}>
